52c5cb88fb604651650aa4f558d9e38f1505216f
Ross Edman <ross.edman@gmail.com>
2024-07-28 13:05:44 -0500

    Reload Fix

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,11 +1,11 @@
 alias cat='bat'
 
 # clear
-alias cl="clear"
-alias ckear="clear"
-alias clr="clear"
+# alias cl="clear"
+# alias ckear="clear"
+# alias clr="clear"
 
 # history
-alias h="history 100 | fzf"
-alias h2="history 200 | fzf"
-alias h3="history 300 | fzf"
+# alias h="history 100 | fzf"
+# alias h2="history 200 | fzf"
+# alias h3="history 300 | fzf"
