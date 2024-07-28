b4e202a6be7ebfa13351f1be8707020570c1c648
Ross Edman <ross.edman@gmail.com>
2024-07-28 12:56:38 -0500

    Add Aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,1 +1,20 @@
 alias cat='bat'
+
+# clear
+alias c="clear"
+alias cl="clear"
+alias ckear="clear"
+alias clr="clear"
+
+# directory traversal
+alias .="cd .."
+alias ..="cd ../.."
+alias ...="cd ../../.."
+alias ....="cd ../../../.."
+alias .....="cd ../../../../.."
+
+# history
+alias h="history | fzf"
+alias h1="history 10"
+alias h2="history 20"
+alias h3="history 30"
