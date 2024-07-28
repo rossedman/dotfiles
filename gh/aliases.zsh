af3e068186f491b3e9f83bd9e154574372c3b97f
Ross Edman <ross.edman@gmail.com>
2024-07-28 13:11:59 -0500

    Add GH Aliases

--- gh/aliases.zsh
+++ gh/aliases.zsh
@@ -1,2 +1,1 @@
-alias ghlr="gh release list -L 1 --json tagName --jq '.[].tagName'"
-alias ghlrv="gh release view $(ghlr)"
+gh alias set rl 'release list -L 1 --json tagName --jq ".[].tagName"' --clobber
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,11 +1,1 @@
 alias cat='bat'
-
-# clear
-# alias cl="clear"
-# alias ckear="clear"
-# alias clr="clear"
-
-# history
-# alias h="history 100 | fzf"
-# alias h2="history 200 | fzf"
-# alias h3="history 300 | fzf"
