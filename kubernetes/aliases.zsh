65697ce813b0e2789d46cae141d0f4030a4260b1
Ross Edman <ross.edman@gmail.com>
2024-07-28 19:58:20 -0500

    Moar Kubernetes

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,17 +1,19 @@
 alias k="kubectl"
 alias k9s="k9s --logoless"
+alias kcc="kubectl config current-context"
 alias kgn="kubectl get nodes"
 alias kgp="kubectl get pods"
+
+# my extras
+alias kenid="kubectl extras nodes-by-id"
+alias kenip="kubectl extras nodes-by-ip"
+alias kenaws="kubectl extras nodes-by-aws"
 
 kfuzz() {
     kubectl --no-headers "$@" | fzf
 }
 
-kchoose() {
+kctx() {
     context=$(kubectl config get-contexts --no-headers -o name | gum choose)
     kubectl config use-context $context
-}
-
-kubectl-extras-node_by_id() {
-    kubectl get nodes -o=custom-columns='NAME:.metadata.name, STATUS:status.conditions[-1].type, ID:.spec.providerID' "$@"
 }
