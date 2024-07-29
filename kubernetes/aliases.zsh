05ed55715c95fac6b6d23ca7ef898d6be6e89f62
Ross Edman <ross.edman@gmail.com>
2024-07-28 19:40:03 -0500

    Move Functions

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -2,3 +2,16 @@ alias k="kubectl"
 alias k9s="k9s --logoless"
 alias kgn="kubectl get nodes"
 alias kgp="kubectl get pods"
+
+kfuzz() {
+    kubectl --no-headers "$@" | fzf
+}
+
+kchoose() {
+    context=$(kubectl config get-contexts --no-headers -o name | gum choose)
+    kubectl config use-context $context
+}
+
+kubectl-extras-node_by_id() {
+    kubectl get nodes -o=custom-columns='NAME:.metadata.name, STATUS:status.conditions[-1].type, ID:.spec.providerID' "$@"
+}
--- kubernetes/functions.zsh
+++
@@ -1,8 +0,0 @@
-kfuzz() { 
-    kubectl --no-headers "$@" | fzf 
-}
-
-kchoose() {
-    context=$(kubectl config get-contexts --no-headers -o name | gum choose)
-    kubectl config use-context $context
-}
